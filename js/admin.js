7a0ef7d60b9600fc7c8fd1dca8e9e0fd9ac7ae49
Chris Burgess <chris@giantrobot.co.nz>
2011-01-11 23:24:43 +1300

    [#891128] and [#884724].
    Some improvements to the flow based on Zohar's comments on issue queues.
    
    Registration works against www.kaltura.com
    
    Registering against CE (Kaltura-supplied AMI image) failing with MySQL errors, but will discuss with ZB.
    
    Need addition of login for those with existing partner details and we're ready to get this tested.

--- js/admin.js
+++ js/admin.js
@@ -1,18 +1,19 @@
 Drupal.kalturaAdmin = {
     
     init: function() {
-        if ( $('#edit-server-url').val() == 'http://www.kaltura.com' && $('input[@name=kaltura_registration_mode]:checked').val() == 1 ) {
-            $('#fieldset-kaltura-ce').hide() ;
-            $('#fieldset-kaltura-saas').show() ;
+        if ( $('#saas-or-ce-wrapper').length ) {
+            if ( $('#saas-or-ce-wrapper input[@name=saas_or_ce]:checked').val() != 'ce' ) {
+                $('#edit-kaltura-url-wrapper').hide('slow');
+            }
+            $('#saas-or-ce-wrapper input[@name=saas_or_ce]').click( function() {
+                    if ( $('#saas-or-ce-wrapper input[@name=saas_or_ce]:checked').val() != 'ce' ) {
+                        $('#edit-kaltura-url-wrapper').hide('slow');
+                    }
+                    else {
+                        $('#edit-kaltura-url-wrapper').show('slow');                        
+                    }
+                } );
         }
-        $('#edit-kaltura-registration-mode-0').click( function() {
-            $('#fieldset-kaltura-ce').show() ;
-            $('#fieldset-kaltura-saas').hide() ;
-        } ) ;
-        $('#edit-kaltura-registration-mode-1').click( function() {
-            $('#fieldset-kaltura-ce').hide() ;
-            $('#fieldset-kaltura-saas').show() ;
-        } ) ;
     }
 
 } ;
