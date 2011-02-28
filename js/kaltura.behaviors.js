7ca7872b96965f62bed7fd2a96e9e699b09b50d7
Yaniv Aran-Shamir <yaniv@gambaweb.co.il>
2011-02-28 15:30:02 +0200

    regestration form changes

--- js/kaltura.behaviors.js
+++ js/kaltura.behaviors.js
@@ -1,7 +1,6 @@
 (function ($) {
   Drupal.behaviors.kaltura = {
     attach: function(context, settings) {
-      console.dir(context);
       $('.remove_media', context).click( function () {
         $(this).parents('.kaltura-thumb-wrap').nextAll().children('input:hidden').val('');
         $(this).parents('.kaltura-thumb-wrap').replaceWith('');
